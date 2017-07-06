602c7f15ecce08a57fd84e8ba36a1f69b732a31d
lambert01 <190038225@qq.com>
2017-07-06 14:42:00 +0800

    q

---
+++ src/main/java/com/autoCustomer/entity/TblPropertiesInfo.java
@@ -0,0 +1,75 @@
+package com.autoCustomer.entity;
+
+import java.util.Date;
+
+public class TblPropertiesInfo {
+    private Integer infoid;
+
+    private String kind;
+
+    private String name;
+
+    private String value;
+
+    private String status;
+
+    private Date createdate;
+
+    private Date updatedate;
+
+    public Integer getInfoid() {
+        return infoid;
+    }
+
+    public void setInfoid(Integer infoid) {
+        this.infoid = infoid;
+    }
+
+    public String getKind() {
+        return kind;
+    }
+
+    public void setKind(String kind) {
+        this.kind = kind == null ? null : kind.trim();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value == null ? null : value.trim();
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status == null ? null : status.trim();
+    }
+
+    public Date getCreatedate() {
+        return createdate;
+    }
+
+    public void setCreatedate(Date createdate) {
+        this.createdate = createdate;
+    }
+
+    public Date getUpdatedate() {
+        return updatedate;
+    }
+
+    public void setUpdatedate(Date updatedate) {
+        this.updatedate = updatedate;
+    }
+}
