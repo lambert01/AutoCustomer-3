96a935349ed0a30dd30dd475059c1632ed33a894
lambert01 <190038225@qq.com>
2017-07-06 15:49:14 +0800

    q

--- src/main/java/com/autoCustomer/util/ImageUtil.java
+++ src/main/java/com/autoCustomer/util/ImageUtil.java
@@ -9,19 +9,20 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ImageUtil {
+	private String imagepath = this.getClass().getClassLoader().getResource("image.txt").getPath();
 
 	/**
 	 * 把图片链接放入集合中
 	 * @return
 	 */
-	private static List<String> getHttpLinksByfile(){
+	private  List<String> getHttpLinksByfile(){
 		List<String> arrs = new ArrayList<String>();
 		FileInputStream fis = null;
 		InputStreamReader isr = null;
 		BufferedReader br = null; // 用于包装InputStreamReader,提高处理性能。因为BufferedReader有缓冲的，而InputStreamReader没有。
 		try {
 			String str = "";
-			fis = new FileInputStream("src/main/resources/image.txt");// FileInputStream
+			fis = new FileInputStream(imagepath);// FileInputStream
 			// 从文件系统中的某个文件中获取字节
 			isr = new InputStreamReader(fis, "gb2312");// InputStreamReader  是字节流通向字符流的桥梁,
 			br = new BufferedReader(isr);// 从字符输入流中读取文件中的内容,封装了一个new  InputStreamReader的对象
@@ -56,7 +57,7 @@ public class ImageUtil {
 	/**
 	 * 随机返回集合中的一个链接
 	 */
-	public static String getHttpLink(){
+	public  String getHttpLink(){
 		List<String> arrs = getHttpLinksByfile();
 		int size = arrs.size();
 		int index = (int) (Math.random() * size);
