f0cde2b9d242158f990fe1f72920d4c8cc7e7c6f
lambert01 <190038225@qq.com>
2017-07-06 10:35:45 +0800

    q

---
+++ src/main/java/com/autoCustomer/util/Constant.java
@@ -0,0 +1,50 @@
+package com.autoCustomer.util;
+
+public class Constant {
+	
+	/**
+	 * year 等到当前年份2017
+	 */
+	public static final String YEAR = "2017";
+	
+	
+	/**
+	 * 随机数保留三位有效数字
+	 */
+	
+	public static final String DIGIT = "0.000";
+	
+	
+	/**
+	 * 保留小数位数 2位数
+	 */
+	
+	public static final Integer DIGIT2 = 2;
+	
+	/**
+	 * 日期为居多的时间分布类型2
+	 */
+	public static final String MAJORITY = "2";
+	
+	/**
+	 * 日期为平均分布的时间分布类型1
+	 */
+	public static final String AVERAGE_DATE= "1";
+	
+	/**
+	 * 日期分布类型为居多
+	 */
+	public static final String MAJORITY_DATE = "居多";
+	
+	
+	/**
+	 * 初始来源类型6
+	 */
+	
+	public static final String ACTIVITY = "6";
+	
+	/**
+	 * 当前阶段数据类型7
+	 */
+	public static final String CURRENT_STAGE = "7";
+}
