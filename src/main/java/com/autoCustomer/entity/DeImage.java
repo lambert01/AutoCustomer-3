57aa0dfe535d8de8d0ea5a8a9b56d378e4ec1a84
lambert01 <190038225@qq.com>
2017-07-07 11:31:58 +0800

    q

---
+++ src/main/java/com/autoCustomer/entity/DeImage.java
@@ -0,0 +1,23 @@
+package com.autoCustomer.entity;
+
+public class DeImage {
+    private Integer imageid;
+
+    private String imageurl;
+
+    public Integer getImageid() {
+        return imageid;
+    }
+
+    public void setImageid(Integer imageid) {
+        this.imageid = imageid;
+    }
+
+    public String getImageurl() {
+        return imageurl;
+    }
+
+    public void setImageurl(String imageurl) {
+        this.imageurl = imageurl == null ? null : imageurl.trim();
+    }
+}
