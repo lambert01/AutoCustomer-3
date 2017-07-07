56a69e2c8bcd66877acd519ca29af66c447d0f85
lambert01 <190038225@qq.com>
2017-07-07 17:11:24 +0800

    q

--- src/main/java/com/autoCustomer/util/CommonInterceptor.java
+++ src/main/java/com/autoCustomer/util/CommonInterceptor.java
@@ -21,7 +21,6 @@ public class CommonInterceptor implements HandlerInterceptor {
     }  
   
     /**  
-     *   
      * 在业务处理器处理请求之前被调用 如果返回false   
      * 从当前的拦截器往回执行所有拦截器的afterCompletion(),  
      * 再退出拦截器链, 如果返回true 执行下一个拦截器,  
@@ -29,9 +28,7 @@ public class CommonInterceptor implements HandlerInterceptor {
      * 然后进入拦截器链,  
      * 从最后一个拦截器往回执行所有的postHandle()  
      * 接着再从最后一个拦截器往回执行所有的afterCompletion()  
-     *   
      * @param  request  
-     *   
      * @param  response  
      */  
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response,  
@@ -51,15 +48,11 @@ public class CommonInterceptor implements HandlerInterceptor {
     }  
   
     /**  
-     *   
      * 在DispatcherServlet完全处理完请求后被调用  
      * 当有拦截器抛出异常时,  
      * 会从当前拦截器往回执行所有的拦截器的afterCompletion()  
-     *   
      * @param request  
-     *   
      * @param response  
-     *   
      * @param handler  
      *   
      */  
