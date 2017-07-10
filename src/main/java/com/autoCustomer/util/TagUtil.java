d3477d5f71cf6ebf1a2d909dd9a70e79200a1994
lambert01 <190038225@qq.com>
2017-07-10 15:59:05 +0800

    q

--- src/main/java/com/autoCustomer/util/TagUtil.java
+++ src/main/java/com/autoCustomer/util/TagUtil.java
@@ -19,14 +19,14 @@ public class TagUtil {
 	 * 读取标签配置
 	 * @return
 	 */
-	private static Map<String, String> getAllTagsByfile(){
+	private  Map<String, String> getAllTagsByfile(){
 		Map<String, String> tagmap = new HashMap<String, String>();
 		FileInputStream fis = null;
 		InputStreamReader isr = null;
 		BufferedReader br = null; // 用于包装InputStreamReader,提高处理性能。因为BufferedReader有缓冲的，而InputStreamReader没有。
 		try {
 			String str = "";
-			fis = new FileInputStream("src/main/resources/tagSet.txt");// FileInputStream
+			fis = new FileInputStream(this.getClass().getClassLoader().getResource("tagSet2.txt").getPath());// FileInputStream
 			// 从文件系统中的某个文件中获取字节
 			isr = new InputStreamReader(fis, "utf-8");// InputStreamReader 是字节流通向字符流的桥梁,
 			br = new BufferedReader(isr);// 从字符输入流中读取文件中的内容,封装了一个new InputStreamReader的对象
@@ -67,7 +67,7 @@ public class TagUtil {
 	/***
 	 * 返回标签
 	 */
-	public static Map<String, Object> getTags(){
+	public  Map<String, Object> getTags(){
 		Map<String, Object> returnmap = new LinkedHashMap<String, Object>();
 		Map<String, Object> returnmap2 = new LinkedHashMap<String, Object>();
 		Map<String,String> allTagsmap = getAllTagsByfile();
