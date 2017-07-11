b17acccf39eb14ea242df6306f3bef0bd8668eb1
lambert01 <190038225@qq.com>
2017-07-11 11:13:24 +0800

    q

--- src/main/java/com/autoCustomer/util/TagUtil.java
+++ src/main/java/com/autoCustomer/util/TagUtil.java
@@ -1,10 +1,5 @@
 package com.autoCustomer.util;
 
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
@@ -15,44 +10,6 @@ import java.util.regex.Pattern;
 
 public class TagUtil {
 
-	/**
-	 * 读取标签配置
-	 * @return
-	 */
-	private  Map<String, String> getAllTagsByfile(){
-		Map<String, String> tagmap = new HashMap<String, String>();
-		FileInputStream fis = null;
-		InputStreamReader isr = null;
-		BufferedReader br = null; // 用于包装InputStreamReader,提高处理性能。因为BufferedReader有缓冲的，而InputStreamReader没有。
-		try {
-			String str = "";
-			fis = new FileInputStream(this.getClass().getClassLoader().getResource("tagSet2.txt").getPath());// FileInputStream
-			// 从文件系统中的某个文件中获取字节
-			isr = new InputStreamReader(fis, "utf-8");// InputStreamReader 是字节流通向字符流的桥梁,
-			br = new BufferedReader(isr);// 从字符输入流中读取文件中的内容,封装了一个new InputStreamReader的对象
-			while ((str = br.readLine())!=null){
-				String[] strs = str.split(":");
-				String dimension = strs[0].trim();
-				String name = strs[1].trim();
-				tagmap.put(name, dimension);
-
-			}
-		}catch(FileNotFoundException e){
-			System.out.println("找不到指定文件");
-		}catch(IOException e){
-			System.out.println("读取文件失败");
-		}finally{
-			try{
-				br.close();
-				isr.close();
-				fis.close();
-				// 关闭的时候最好按照先后顺序关闭最后开的先关闭所以先关s,再关n,最后关m
-			}catch(IOException e){
-				e.printStackTrace();
-			}
-		}
-		return tagmap;
-	}
 
 	/**
 	 * 决定返回几个标签
