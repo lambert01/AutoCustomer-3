2686dd354cacc0ef78e6acab747691fe5641fe54
lambert01 <190038225@qq.com>
2017-07-12 10:39:33 +0800

    q

--- src/main/java/com/autoCustomer/service/AddcustomerService.java
+++ src/main/java/com/autoCustomer/service/AddcustomerService.java
@@ -3,7 +3,7 @@ package com.autoCustomer.service;
 import net.sf.json.JSONObject;
 
 public interface AddcustomerService {
-	public String addcustomer(String data);
+	public String addcustomer();
 	public  JSONObject getcustomer();
 
 }
