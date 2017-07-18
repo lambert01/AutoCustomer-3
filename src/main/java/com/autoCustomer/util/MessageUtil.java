f909c25965025c89e6e2c0f1efe863bc2d04347e
lambert01 <190038225@qq.com>
2017-07-18 19:08:20 +0800

    q

--- src/main/java/com/autoCustomer/util/MessageUtil.java
+++ src/main/java/com/autoCustomer/util/MessageUtil.java
@@ -92,4 +92,21 @@ public class MessageUtil {
 		return openId;
 	}
 	
+	/**
+	 * 返回一个随机的订单号
+	 * @return
+	 */
+	public static String getOrderNumber(){
+		StringBuilder str=new StringBuilder();//定义变长字符串
+		Random random=new Random();
+		//随机生成数字，并添加到字符串
+		for(int i=0;i<18;i++){
+		    str.append(random.nextInt(10));
+		}
+		//将字符串转换为数字并输出
+		String num=str.toString();
+		System.out.println(num);
+		return num;
+	}
+	
 }
