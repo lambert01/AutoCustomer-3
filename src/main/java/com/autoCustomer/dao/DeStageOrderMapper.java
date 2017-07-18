c40000528097c1ed6c027ffb7594d7b562e06090
lambert01 <190038225@qq.com>
2017-07-18 17:55:05 +0800

    q

--- src/main/java/com/autoCustomer/dao/DeStageOrderMapper.java
+++ src/main/java/com/autoCustomer/dao/DeStageOrderMapper.java
@@ -14,4 +14,7 @@ public interface DeStageOrderMapper {
     int updateByPrimaryKeySelective(DeStageOrder record);
 
     int updateByPrimaryKey(DeStageOrder record);
+    
+    //通过stageid,客户状态id查找该状态是否有订单
+    Integer selectByStageId(Integer stageid);
 }
