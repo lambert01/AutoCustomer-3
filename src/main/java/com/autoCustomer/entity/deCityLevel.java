5c60e23c45b09becd9ae7b85c66912783b623ce2
lambert01 <190038225@qq.com>
2017-07-19 15:43:33 +0800

    q

---
+++ src/main/java/com/autoCustomer/entity/deCityLevel.java
@@ -0,0 +1,33 @@
+package com.autoCustomer.entity;
+
+public class deCityLevel {
+    private Integer id;
+
+    private String city;
+
+    private String level;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city == null ? null : city.trim();
+    }
+
+    public String getLevel() {
+        return level;
+    }
+
+    public void setLevel(String level) {
+        this.level = level == null ? null : level.trim();
+    }
+}
