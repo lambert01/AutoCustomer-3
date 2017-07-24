eac58e9a4ae0600b6d8147b6f22bef1a5966fe25
lambert01 <190038225@qq.com>
2017-07-24 12:00:38 +0800

    q

---
+++ src/main/java/com/autoCustomer/dao/DeEventTagMapper.java
@@ -0,0 +1,21 @@
+package com.autoCustomer.dao;
+
+import java.util.List;
+
+import com.autoCustomer.entity.DeEventTag;
+
+public interface DeEventTagMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(DeEventTag record);
+
+    int insertSelective(DeEventTag record);
+
+    DeEventTag selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(DeEventTag record);
+
+    int updateByPrimaryKey(DeEventTag record);
+    
+    List<DeEventTag> selectAllEventTag();
+}
