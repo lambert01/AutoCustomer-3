42d69306d1751312312570998eba7bd0e202599b
lambert01 <190038225@qq.com>
2017-07-26 12:22:33 +0800

    q

---
+++ src/main/java/com/autoCustomer/dao/DeStageEventTargetMapper.java
@@ -0,0 +1,28 @@
+package com.autoCustomer.dao;
+
+import java.util.List;
+
+import com.autoCustomer.entity.DeStageEventTarget;
+
+public interface DeStageEventTargetMapper {
+    int deleteByPrimaryKey(Integer eventid);
+
+    int insert(DeStageEventTarget record);
+
+    int insertSelective(DeStageEventTarget record);
+
+    DeStageEventTarget selectByPrimaryKey(Integer eventid);
+
+    int updateByPrimaryKeySelective(DeStageEventTarget record);
+
+    int updateByPrimaryKey(DeStageEventTarget record);
+    
+    //查询所有符合客户当前以及之前状态对应的事件
+    List<DeStageEventTarget> selectEventsByStage(Integer stageid);
+    
+    //查询与状态无关的事件
+    List<DeStageEventTarget> selectUnRelatedStageEvent();
+     
+    //查询相同阶段的多选事件有几个
+	int selectCountSize(Integer ismultiselect);
+}
