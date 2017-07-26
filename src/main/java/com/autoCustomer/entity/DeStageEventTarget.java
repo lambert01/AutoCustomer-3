9ff8f9faeceadd9dafe8998567e7d810bd14523a
lambert01 <190038225@qq.com>
2017-07-26 12:22:43 +0800

    q

---
+++ src/main/java/com/autoCustomer/entity/DeStageEventTarget.java
@@ -0,0 +1,83 @@
+package com.autoCustomer.entity;
+
+public class DeStageEventTarget {
+    private Integer eventid;
+
+    private String event;
+
+    private String eventname;
+
+    private Integer stageid;
+
+    private String targetid;
+
+    private String targetname;
+
+    private Integer isrelated;
+
+    private Integer ismultiselect;
+
+    public Integer getEventid() {
+        return eventid;
+    }
+
+    public void setEventid(Integer eventid) {
+        this.eventid = eventid;
+    }
+
+    public String getEvent() {
+        return event;
+    }
+
+    public void setEvent(String event) {
+        this.event = event == null ? null : event.trim();
+    }
+
+    public String getEventname() {
+        return eventname;
+    }
+
+    public void setEventname(String eventname) {
+        this.eventname = eventname == null ? null : eventname.trim();
+    }
+
+    public Integer getStageid() {
+        return stageid;
+    }
+
+    public void setStageid(Integer stageid) {
+        this.stageid = stageid;
+    }
+
+    public String getTargetid() {
+        return targetid;
+    }
+
+    public void setTargetid(String targetid) {
+        this.targetid = targetid == null ? null : targetid.trim();
+    }
+
+    public String getTargetname() {
+        return targetname;
+    }
+
+    public void setTargetname(String targetname) {
+        this.targetname = targetname == null ? null : targetname.trim();
+    }
+
+    public Integer getIsrelated() {
+        return isrelated;
+    }
+
+    public void setIsrelated(Integer isrelated) {
+        this.isrelated = isrelated;
+    }
+
+    public Integer getIsmultiselect() {
+        return ismultiselect;
+    }
+
+    public void setIsmultiselect(Integer ismultiselect) {
+        this.ismultiselect = ismultiselect;
+    }
+}
