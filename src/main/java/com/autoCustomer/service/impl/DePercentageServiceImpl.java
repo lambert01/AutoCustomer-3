40e98f295d35bcae363e58c82a1b0ae04c2f2a3c
lambert01 <190038225@qq.com>
2017-07-27 14:12:37 +0800

    q

--- src/main/java/com/autoCustomer/service/impl/DePercentageServiceImpl.java
+++ src/main/java/com/autoCustomer/service/impl/DePercentageServiceImpl.java
@@ -171,7 +171,7 @@ public class DePercentageServiceImpl implements DePercentageService{
 				if(num > start && num <= end){
 					state_date = ((String) (monthList.get(i).get("start_date"))).split(":")[0];
 					end_date = ((String) (monthList.get(i).get("end_date"))).split(":")[0];
-					time = getRanTime(Integer.valueOf(state_date),Integer.valueOf(end_date))+":00:00";
+					time = getRanTime(Integer.valueOf(state_date),Integer.valueOf(end_date))+":"+getRanMinute()+":"+getRanMinute();
 				}
 			}
 		}else{
@@ -183,7 +183,7 @@ public class DePercentageServiceImpl implements DePercentageService{
 				if(num > start && num <= end){
 					state_date = ((String) (monthList.get(i).get("start_date"))).split(":")[0];
 					end_date = ((String) (monthList.get(i).get("end_date"))).split(":")[0];
-					time = getRanTime(Integer.valueOf(state_date),Integer.valueOf(end_date))+":00:00";
+					time = getRanTime(Integer.valueOf(state_date),Integer.valueOf(end_date))+":"+getRanMinute()+":"+getRanMinute();
 					break;
 				}
 			}
@@ -236,5 +236,14 @@ public class DePercentageServiceImpl implements DePercentageService{
 		public static String getString(String str){
 			return String.valueOf(Integer.parseInt(str));
 		}
+		
+		public String getRanMinute(){
+		    //DecimalFormat df = new DecimalFormat("00");
+		    Random ran = new Random();
+		    String[] str = "00,01,02,03,04,05,06,07,08,09,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,52,52,53,54,55,56,56,58,59".split(",");
+		    int num = ran.nextInt(60);
+		    String strTime = str[num];
+		    return strTime;
+		  }
 		
 }
