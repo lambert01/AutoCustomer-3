26f5b5587a867278d6207558c1522d457a3d1c56
lambert01 <190038225@qq.com>
2017-07-27 14:11:53 +0800

    q

--- src/main/java/com/autoCustomer/service/AddcustomerService.java
+++ src/main/java/com/autoCustomer/service/AddcustomerService.java
@@ -1,6 +1,12 @@
 package com.autoCustomer.service;
 
 public interface AddcustomerService {
-	public String addcustomer(String username);
+	public String addcustomer(String username,String token);
+
+	String getAccessToken(String username);
+
+	String getPropertyInfo(String kind);
+
+	String gettaglist(String access_token);
 
 }
