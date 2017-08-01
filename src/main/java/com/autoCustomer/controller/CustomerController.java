7e69ad4aee3592c6cb24b8c73e5d64ea76de7122
lambert01 <190038225@qq.com>
2017-08-01 18:04:34 +0800

    q

--- src/main/java/com/autoCustomer/controller/CustomerController.java
+++ src/main/java/com/autoCustomer/controller/CustomerController.java
@@ -1,20 +1,17 @@
 package com.autoCustomer.controller;
 
+import java.util.HashMap;
 import java.util.Map;
-
-import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.autoCustomer.entity.DePropertiesInfo;
 import com.autoCustomer.service.AddcustomerService;
-import com.autoCustomer.service.DePercentageService;
 
-import net.sf.json.JSONArray;
 import net.sf.json.JSONObject;
 
 @Controller
@@ -22,38 +19,8 @@ import net.sf.json.JSONObject;
 public class CustomerController {
 	
 	@Autowired
-	private DePercentageService percentageService;
-	
-	@Autowired
-	private AddcustomerService addcustomerService2;
-	
-	@RequestMapping(value="/createTime",method= RequestMethod.GET,produces = "application/json;charset=UTF-8")
-	@ResponseBody
-	public String excete(HttpServletResponse response) {
-		JSONObject json = new JSONObject();
-		//response.setContentType("application/json;charset=UTF-8");
-		response.setHeader("Content-Type", "application/xml; charset=UTF-8");  
-		String createTime = percentageService.frontPercentage();
-		System.out.println("客户的创建时间是"+createTime);
-		System.out.println("createTime:"+createTime);
-		for (int i = 0; i < 10; i++) {
-			Map<String, Object> currentStages = percentageService.getCurrentStage();
-			String meaage = currentStages.get("message").toString();
-			System.out.println("客户状态是 "+meaage);
-			percentageService.getRanCreateTime();
-		}
-		
-		//System.out.println("currentStage:"+currentStage);
-		String activeDate = percentageService.getActiveData();
-		System.out.println("activeDate:"+activeDate);
-		json.put("createTime", createTime);
-	//	json.put("currentStage",currentStage);
-		json.put("activeDate", activeDate);
-		return json.toString();
-	}
-	
+	private AddcustomerService addcustomerService;
  
-	
     /**
      * 自动创建客户
      * @param size
@@ -62,30 +29,38 @@ public class CustomerController {
 	@RequestMapping(value="/createCustomer/{username}/{size}",produces = "text/json;charset=UTF-8")
 	@ResponseBody
 	public String createCustomer(@PathVariable("username")String username,@PathVariable("size")int size){
-		JSONArray arr = new JSONArray();
+		JSONObject returnjson = new JSONObject();
+		
 		if(size <= 0){
-			size = 1;
+			returnjson.put("error", "至少创建一个用户");
+			return returnjson.toString();
 		}
-		String  token = addcustomerService2.getAccessToken(username);
+		
+		String  token = addcustomerService.getAccessToken(username);
 		
 		if(token == null || "".equals(token)){
-			return "没有对应的token";
+			returnjson.put("error", "没有对应的token");
+			return returnjson.toString();
 		}
 		
-		addcustomerService2.gettaglist(token);
-		
-		int countsize = 0;
-		for (int i = 0; i < size; i++) { 
-			
-			String mes = addcustomerService2.addcustomer(username,token);
-			countsize++;
-			if(countsize == 500){
-				token = addcustomerService2.getAccessToken(username);
-			}
-			arr.add(mes);
+		DePropertiesInfo info = addcustomerService.selectallPropertyInfoByKind(username);
+		String accounttype = info.getStatus();
+		Integer userid = info.getInfoid();
+		if(accounttype == null || "".equals(accounttype)){
+			returnjson.put("error", "帐号没有配置对应的类型");
+			return returnjson.toString();
 		}
-		String retnemmes = arr.toString();
 		
-		return retnemmes;
+		//addcustomerService.gettaglist(token,accounttype);
+		 
+		Map<String, Object> paramap = new HashMap<String, Object>();
+		paramap.put("username", username); //用户名
+		paramap.put("accounttype", accounttype);//帐号类型
+		paramap.put("accessToken", token);
+		paramap.put("size", size); //创建几个用户
+		paramap.put("userid", userid); //帐号的id,数据库中没有保存帐号,用唯一的appid的id替代帐号的唯一性
+		String mes = addcustomerService.addcustomer(paramap);
+ 
+		return mes;
 	}
 }
